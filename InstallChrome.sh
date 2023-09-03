c8eefb7bab4951cd80294898dafb8b9657d1fa7f
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-09-03 12:01:36 +0530

    Update InstallChrome.sh

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -6,10 +6,10 @@ sudo apt-get update
 sudo apt-get install google-chrome-stable
 wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod 777 /usr/bin/chromedriver
+sudo mv chromedriver /usr/bin/google-chrome
+sudo chown root:root /usr/bin/google-chrome
+sudo chmod 777 /usr/bin/google-chrome
 wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.2.jar
 mv selenium-server-4.1.2.jar selenium-server.jar
 #sudo apt install xvfb
-xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar selenium-server.jar
+xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/google-chrome -jar selenium-server.jar
