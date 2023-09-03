44575ab304a14144dad1502f65db937dc2afe7a9
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-09-03 11:09:46 +0530

    Create Install_Chrome.sh

---
+++ Install_Chrome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt install default-jdk
+sudo curl -sS -o – https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo bash -c “echo ‘deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main’ >> /etc/apt/sources.list.d/google-chrome.list”
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.2.jar
+mv selenium-server-4.1.2.jar selenium-server.jar
+#sudo apt install xvfb
+xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar selenium-server.jar
