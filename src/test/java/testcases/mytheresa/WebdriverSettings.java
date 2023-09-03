a3e131aa46346cf927ef9eee7c4efc28cbe7d874
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-09-03 15:00:10 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -1,14 +1,26 @@
 package testcases.mytheresa;
-
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/google-chrome");
-		WebDriver driver = new ChromeDriver();
-		return driver;
+ WebDriverManager.chromedriver().setup();
+ ChromeOptions options = new ChromeOptions();
+ options.addArguments("--no-sandbox");
+ options.addArguments("--disable-dev-shm-usage");
+ options.addArguments("--headless");
+ driver = new ChromeDriver(options);
+ 
+ driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+ driver.manage().window().maximize();  
+ driver.get(baseUrl);
+ driver.close();
+ return driver;
 	}
 
 }
